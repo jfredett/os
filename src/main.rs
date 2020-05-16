a18834b4e1e00234e096bf9b63c329b16df21340
Joe Fredette <jfredett@gmail.com>
2020-05-16 09:14:33 -0400

    implement panic handler

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ use core::panic::PanicInfo;
 mod vga;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
@@ -15,7 +16,7 @@ fn panic(_info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     let mut i = 0;
     loop {
-        println!("i = {}; i^2 = {}", i, i*i);
+        println!("i = {}; i^3 = {}", i, i*i*i);
         i += 1;
     }
 }
