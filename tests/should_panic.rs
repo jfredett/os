f757585d870caf47d5a4a1c7cf94c1eba06a4c23
Joe Fredette <jfredett@gmail.com>
2020-05-19 23:52:16 -0400

    refactor should_panic.rs to no-harness

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -1,27 +1,16 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(os::test_runner)]
-#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 use os::{QemuExitCode, exit_qemu, serial_print, serial_println};
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    serial_println!("Running {}", file!());
-    test_main();
-
+    serial_print!("Running {}: ", file!());
+    should_fail();
+    serial_println!("[fail]");
+    exit_qemu(QemuExitCode::Failure);
     loop {}
-}
-
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    for test in tests {
-        test();
-        serial_println!("[test did not panic]");
-        exit_qemu(QemuExitCode::Failure);
-    }
-    exit_qemu(QemuExitCode::Success);
 }
 
 #[panic_handler]
@@ -31,8 +20,6 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[test_case]
 fn should_fail() {
-    serial_print!("ensuring panic handler works: ");
     assert!(false);
 }
