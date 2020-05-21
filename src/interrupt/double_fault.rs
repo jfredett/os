75e5b7d1fedd5440765bf099791dc5252c918382
Joe Fredette <jfredett@gmail.com>
2020-05-21 19:43:57 -0400

    remove unneeded test

--- src/interrupt/double_fault.rs
+++ src/interrupt/double_fault.rs
@@ -7,19 +7,3 @@ pub extern "x86-interrupt" fn handler(
     println!("DOUBLE FAULT:");
     panic!("{:#?}", stack_frame);
 }
-
-#[cfg(test)]
-mod tests {
-    use crate::{serial_print, serial_println};
-
-    mod exceptions {
-        use super::*;
-
-        #[test_case]
-        fn breakpoint_exception_works() {
-            serial_print!("testing breakpoint exception: ");
-            x86_64::instructions::interrupts::int3();
-            serial_println!("[ok]");
-        }
-    }
-}
